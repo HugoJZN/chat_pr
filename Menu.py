b3ba3e3da4bc53aa08dd2fb464700f165d6f1aeb
HugoJZN <hugo@juzyna.com>
2023-11-25 22:36:44 +0100

    val

--- Menu.py
+++ Menu.py
@@ -24,16 +24,6 @@ def menu():
     if choice == "1":
         n = int(input("indiquez le nombre de valeurs que vous voulez du nombre de mot non importants dans le repertoire: "))
         print(n_mot_moins_important(n))
-    elif choice == "2":
-        print(TF_IDF_eleve())
-    elif choice == "3":
-        print(word_chirac())
-    elif choice == "4":
-        print(pr_nation())
-    elif choice == "5":
-        print(one_president_climt())
-    elif choice == "6":
-        print(mots_evoque_pr())
     elif choice.upper() == "\x1b":
         print("Au revoir")
         return
