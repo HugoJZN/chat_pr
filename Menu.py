457cfcb1425075ceca457d682f2c76cd2125549c
Deus <151967832+HugoJZN@users.noreply.github.com>
2023-11-25 23:55:34 +0100

    Update Menu.py
    change

--- Menu.py
+++ Menu.py
@@ -7,12 +7,7 @@ def menu():
     print("-"*80)
     # On propose à l'utilisateur de choisir l'option qu'il souhaite exécuter.
     print("Que souhaitez-vous faire ?\n"
-          "- Tapez 1 : Obtenir les mots moins importants des fichiers.\n"
-          "- Tapez 2 : Afficher les mots avec les TF-IDF les plus élevés.\n"
-          "- Tapez 3 : Trouver les mots les plus fréquemment utilisés par le président Chirac.\n"
-          "- Tapez 4 : Obtenir le nom des présidents parlant le plus souvent de la Nation.\n"
           "- Tapez 5 : Savoir quel président a été le premier à parler de l'écologie.\n"
-          "- Tapez 6 : Obtenir l'ensemble des mots importants évoqués par tous les présidents.\n"
           "- Tapez esc : Quitter le menu.")
     choice = input("Choix : ")
 
@@ -21,19 +16,8 @@ def menu():
     print("-"*80)
 
     # Selon le choix de l'utilisateur, on appelle la fonction appropriée.
-    if choice == "1":
-        n = int(input("indiquez le nombre de valeurs que vous voulez du nombre de mot non importants dans le repertoire: "))
-        print(n_mot_moins_important(n))
-    elif choice == "2":
-        print(TF_IDF_eleve())
-    elif choice == "3":
-        print(word_chirac())
-    elif choice == "4":
-        print(pr_nation())
-    elif choice == "5":
+    if choice == "5":
         print(one_president_climt())
-    elif choice == "6":
-        print(mots_evoque_pr())
     elif choice.upper() == "\x1b":
         print("Au revoir")
         return
